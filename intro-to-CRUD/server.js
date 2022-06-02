fc00017310f699a9f926052c7ca6c19a3f588265
emlegweak <emilythearmstrong@gmail.com>
2022-06-02 18:31:57 -0400

    better go catch it

--- intro-to-CRUD/server.js
+++ intro-to-CRUD/server.js
@@ -74,7 +74,7 @@ MongoClient.connect(connectionString, { useUnifiedTopology: true })
         const isProduction = process.env.NODE_ENV === 'production'
         const port = isProduction ? 7500 : 3000
         app.listen(port, function () {
-            console.log(`listening on ${port}`)
+            console.log(`Server is running on PORT ${port}, better go catch it...`)
         })
     })
     .catch(console.error)
